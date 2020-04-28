9efea341ac4b4df8353d9ed3a3c5878725584cb2
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-04-28 09:51:54 +0200

    Implemented __len__, __getitem__ and constructor

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -1,3 +1,4 @@
+
 from torchvision.datasets import VisionDataset
 
 from PIL import Image
@@ -6,13 +7,11 @@ import os
 import os.path
 import sys
 
-
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
-
 
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
@@ -30,30 +29,62 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
-    def __getitem__(self, index):
+        #Generate a list containing all the classes available in the dataset and a list of index for the classes
+        classes, class_to_idx = self._find_classes(self.root)
+
+        self.loader = loader
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+
+        self.classes_to_idx.remove( classes.index('BACKGROUND_Google'))
+        self.classes.remove('BACKGROUND_Google') 
+
+        #Open and read the file containing all the elements of the split set
+        pathsplit = root + split + ".txt"
+        f = open(pathsplit, 'r')
+        lines = f.readlines()
+
+        items = []
+        items_as_string = []
+        #Generate a list of elements name
+        for line in Lines:
+            if(line.split("/")[0] != "BACKGROUND_Google") :
+              items_as_string.append(line)
+              items.append(pil_loader( root + "/101_ObjectCategories/" + line))
+        f.close()
+
+def __getitem__(self, index):
         '''
         __getitem__ should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
+        #Find the string (label + img_name) corrispondent to index passed as input
+        item = self.items_as_string[index]
+        
+        #Divide the item into label (that is the class) and image name
+        label, image_name = item.split("/")  
 
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+        #By the index, access directly the img file
+        image = self.items[index]    
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
             image = self.transform(image)
 
-        return image, label
+        #By the label, return the index of the class in the list of all the classes for the dataset
+        target = self.classes.index(label)
 
-    def __len__(self):
+        return image, target
+
+def __len__(self):
         '''
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+        length = len(self.items)
         return length
+
+
