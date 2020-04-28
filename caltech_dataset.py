6e941f7a428a20d452d0e93df5f3c497ff4741b0
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-04-28 10:25:29 +0200

    fixed error

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -35,10 +35,10 @@ class Caltech(VisionDataset):
         items_as_string = []
         #Generate a list of elements name
         for line in lines:
-            line.rstrip('\n')
-            if(line.split("/")[0] != "BACKGROUND_Google") :
-              items_as_string.append(line)
-              items.append(pil_loader( root + "/101_ObjectCategories/" + line))
+            ln = line.replace('\n','')
+            if(ln.split("/")[0] != "BACKGROUND_Google") :
+              items_as_string.append(ln)
+              items.append(pil_loader( root + "/101_ObjectCategories/" + ln))
         f.close()
   
 
